983f4fa40c5dd4d5af3934f4036c7d45b744857a
Willem004 <willemstigter004@gmail.com>
2023-10-02 15:35:22 +0200

    first prototype of the motor function

--- Boebot-Lasergamen-Slave/src/IO-layer.cpp
+++ Boebot-Lasergamen-Slave/src/IO-layer.cpp
@@ -1,3 +1,33 @@
 #include "IO-layer.h"
 
+uint updateMotor(motor currentMotor, int motorPower)
+{
+    uint returnValue = 0;
+    // check if motorPower exceeds it's bounds of 1000 to -1000, if so change returnValue accordingly
+    if(-1000 > motorPower || 1000 < motorPower)
+    {
+        if(0 > motorPower)
+        {
+            motorPower = -1000;
+            //returnValue = updateMotor_exceeded_lower_bound_of_motorPower;
+        }
+        else
+        {
+            motorPower = 1000;
+            //returnValue = updateMotor_exceeded_upper_bound_of_motorPower;
+        }
+    }
 
+    // any motor added to this switch case should also be added to the enum "motor in IO-layer.h"
+    switch(currentMotor)
+    {
+        case motorRechts:
+            break;
+        case motorLinks:
+            break;
+        default:
+            //returnValue = updateMotor_invalid_value_for_currentMotor;
+            break;
+    }
+    return returnValue;
+}
--- Boebot-Lasergamen-Slave/src/IO-layer.h
+++ Boebot-Lasergamen-Slave/src/IO-layer.h
@@ -3,6 +3,10 @@
 
 #include "main.h"
 
-
+// any motor added to the system should be added to this enum "motor" and the switch case in the "updateMotor" function in IO-layer.cpp
+enum motor { motorRechts, motorLinks};//, motorTurret };
+// this function accepts values from -1000 to 1000 and sets the motor to this value
+// values outside this range are clamped to stay within valid values
+uint updateMotor(motor currentMotor, int motorPower);
 
 #endif 
