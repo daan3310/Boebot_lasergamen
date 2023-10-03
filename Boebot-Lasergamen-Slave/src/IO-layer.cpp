eb3bc25ca91e1634a5adb8740806eaf943c2c32a
Willem004 <willemstigter004@gmail.com>
2023-10-03 12:25:28 +0200

    first tested version of the motor driver code

--- Boebot-Lasergamen-Slave/src/IO-layer.cpp
+++ Boebot-Lasergamen-Slave/src/IO-layer.cpp
@@ -1,21 +1,44 @@
 #include "IO-layer.h"
 
+Servo myMotorTurret;
+
+uint initMotors(int timer)
+{
+    uint returnValue = 0;
+    
+    ESP32PWM::allocateTimer(timer);
+    myMotorTurret.setPeriodHertz(50);
+    myMotorTurret.attach(motorTurretPWM, 1000, 2000);
+    
+    digitalWrite(motorRechtsPWM, LOW);  // set speed to 0
+    digitalWrite(motorRechtsDIR, HIGH); // set direction to forward
+    digitalWrite(motorLinksPWM, LOW);   // set speed to 0
+    digitalWrite(motorLinksDIR, HIGH);  // set direction to forward
+
+    pinMode(motorRechtsPWM, OUTPUT);
+    pinMode(motorRechtsDIR, OUTPUT);
+    pinMode(motorLinksPWM, OUTPUT);
+    pinMode(motorLinksDIR, OUTPUT);
+
+    return returnValue;
+}
+
 uint updateMotor(motor currentMotor, int motorPower)
 {
     uint returnValue = 0;
     bool direction = (0 <= motorPower);
 
     // check if motorPower exceeds it's bounds of 1000 to -1000, if so change returnValue accordingly
-    if(-1000 > motorPower || 1000 < motorPower)
+    if(-1024 > motorPower || 1024 < motorPower)
     {
         if(0 == direction)
         {
-            motorPower = -1000;
+            motorPower = -1024;
             //returnValue = updateMotor_exceeded_lower_bound_of_motorPower;
         }
         else
         {
-            motorPower = 1000;
+            motorPower = 1024;
             //returnValue = updateMotor_exceeded_upper_bound_of_motorPower;
         }
     }
@@ -24,8 +47,15 @@ uint updateMotor(motor currentMotor, int motorPower)
     switch(currentMotor)
     {
         case motorRechts:
+            analogWrite(motorRechtsPWM, abs(motorPower) / 4);
+            digitalWrite(motorRechtsDIR, direction); // might need to add ! to invert the direction
             break;
         case motorLinks:
+            analogWrite(motorLinksPWM, abs(motorPower) / 4);
+            digitalWrite(motorLinksDIR, direction); // might need to add ! to invert the direction
+            break;
+        case motorTurret:
+            myMotorTurret.write(1500 + (motorPower / 2));
             break;
         default:
             //returnValue = updateMotor_invalid_value_for_currentMotor;
--- Boebot-Lasergamen-Slave/src/IO-layer.h
+++ Boebot-Lasergamen-Slave/src/IO-layer.h
@@ -2,11 +2,18 @@
 #define IO_LAYER_H
 
 #include "main.h"
+#include <ESP32Servo.h>
 
 // any motor added to the system should be added to this enum "motor" and the switch case in the "updateMotor" function in IO-layer.cpp
-enum motor { motorRechts, motorLinks};//, motorTurret };
-// this function accepts values from -1000 to 1000 and sets the motor to this value
+enum motor { motorRechts, motorLinks, motorTurret };
+#define motorRechtsPWM 5
+#define motorRechtsDIR 19
+#define motorLinksPWM 23
+#define motorLinksDIR 18
+#define motorTurretPWM 13
+// this function accepts values from -1024 to 1024 and sets the motor to this value
 // values outside this range are clamped to stay within valid values
+uint initMotors(int timer);
 uint updateMotor(motor currentMotor, int motorPower);
 
 #endif 
