4e3636f76f42f6d8cd51d5f783e1ad5df20c81ce
JJMeneer <Juliusvanvalkengoed@gmail.com>
2024-01-08 12:03:56 +0100

    Final esp-cam feature!

--- Boebot-Lasergamen-camera/src/main.cpp
+++ Boebot-Lasergamen-camera/src/main.cpp
@@ -4,7 +4,7 @@
 #include "soc/rtc_cntl_reg.h"
 #include "esp_camera.h"
 
-#define DEBUG
+//#define DEBUG
 
 const char* ssid = "Leaphy Lasergame!";
 const char* password = "Leaphydebug1!";
@@ -16,7 +16,7 @@ String serverPath = "/upload";  // Flask upload route
 
 const int serverPort = 5000;
 
-const int timerInterval = 10000;    // time (milliseconds) between each HTTP POST image
+const int timerInterval = 1000;    // time (milliseconds) between each HTTP POST image
 unsigned long previousMillis = 0;   // last time image was sent
 
 WiFiClient client;
@@ -40,7 +40,7 @@ WiFiClient client;
 #define HREF_GPIO_NUM     23
 #define PCLK_GPIO_NUM     22
 
-String sendPhoto();
+void sendPhoto();
 
 void setup() {
   WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); 
@@ -51,7 +51,9 @@ void setup() {
   Serial.print("Connecting to ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);  
-  while (WiFi.status() != WL_CONNECTED) {
+
+  while (WiFi.status() != WL_CONNECTED) 
+  {
     Serial.print(".");
     delay(500);
   }
@@ -74,8 +76,8 @@ void setup() {
   config.pin_pclk = PCLK_GPIO_NUM;
   config.pin_vsync = VSYNC_GPIO_NUM;
   config.pin_href = HREF_GPIO_NUM;
-  config.pin_sscb_sda = SIOD_GPIO_NUM;
-  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_sccb_sda = SIOD_GPIO_NUM;
+  config.pin_sccb_scl = SIOC_GPIO_NUM;
   config.pin_pwdn = PWDN_GPIO_NUM;
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
@@ -83,12 +85,12 @@ void setup() {
 
   // init with high specs to pre-allocate larger buffers
   if(psramFound()){
-    config.frame_size = FRAMESIZE_SVGA;
-    config.jpeg_quality = 10;  //0-63 lower number means higher quality
+    config.frame_size = FRAMESIZE_VGA;
+    config.jpeg_quality = 64;  //0-63 lower number means higher quality  // maybe this one?
     config.fb_count = 2;
   } else {
     config.frame_size = FRAMESIZE_CIF;
-    config.jpeg_quality = 12;  //0-63 lower number means higher quality
+    config.jpeg_quality = 32;  //0-63 lower number means higher quality  // 12 121ms    // 32 100ms // 64 50ms
     config.fb_count = 1;
   }
   
@@ -100,52 +102,44 @@ void setup() {
     ESP.restart();
   }
 
-  sendPhoto(); 
+  //sendPhoto(); 
 }
 
 void loop() {
   unsigned long currentMillis = millis();
+  
   if (currentMillis - previousMillis >= timerInterval) 
   {
-    currentMillis = millis();
-    Serial.print("Time before:");
-    Serial.println(currentMillis);
+    unsigned long benchmarkMillisPrev = millis();
     sendPhoto();
-    currentMillis = millis();
-    Serial.print("Time after:");
-    Serial.println(currentMillis);
+    unsigned long benchmarkMillisCurr = millis();
+
+    //Serial.print("Time to send:");
+    Serial.println(benchmarkMillisCurr - benchmarkMillisPrev);
+
     previousMillis = currentMillis;
   }
 }
 
-String sendPhoto() {
-  String getAll;
-  String getBody;
+char header[] = "--ESP32\r\nContent-Disposition: form-data; name=\"image\"; filename=\"1\"\r\nContent-Type: image\r\n\r\n";
+char footer[] = "\r\n--ESP32--\r\n";
 
-  camera_fb_t *fb = NULL;
-  fb = esp_camera_fb_get();
-  if (!fb) {
-    Serial.println("Camera capture failed");
-    delay(1000);
-    ESP.restart();
-  }
-  #ifdef DEBUG  
+void sendPhoto() {
+  camera_fb_t *fb = esp_camera_fb_get();
+
+  #ifdef DEBUG
   Serial.println("Connecting to server: " + serverName);
-  #endif 
+  #endif
 
   if (client.connect(serverName.c_str(), serverPort)) {
-    
     #ifdef DEBUG
     Serial.println("Connection successful!");
     #endif
 
-    String head = "--ESP32\r\nContent-Disposition: form-data; name=\"image\"; filename=\"1\"\r\nContent-Type: image\r\n\r\n";
-    String tail = "\r\n--ESP32--\r\n";
+    size_t imageLen = fb->len;
+    size_t extraLen = sizeof(header) + sizeof(footer) - 2; // Exclude null terminator
+    size_t totalLen = imageLen + extraLen;
 
-    uint16_t imageLen = fb->len;
-    uint16_t extraLen = head.length() + tail.length();
-    uint16_t totalLen = imageLen + extraLen;
-    
     #ifdef DEBUG
     Serial.println("Sending HTTP POST request...");
     #endif
@@ -155,64 +149,38 @@ String sendPhoto() {
     client.println("Content-Length: " + String(totalLen));
     client.println("Content-Type: multipart/form-data; boundary=ESP32");
     client.println();
-    client.print(head);
 
+    client.write(header, sizeof(header) - 1);
+
+    // Sending chunks of data to the Raspberry Pi
     uint8_t *fbBuf = fb->buf;
     size_t fbLen = fb->len;
 
-    for (size_t n = 0; n < fbLen; n = n + 1024) {
-      if (n + 1024 < fbLen) {
-        client.write(fbBuf, 1024);
-        fbBuf += 1024;
-      } else if (fbLen % 1024 > 0) {
-        size_t remainder = fbLen % 1024;
-        client.write(fbBuf, remainder);
-      }
+    for (size_t n = 0; n < fbLen; ) {
+      size_t chunkSize = (1024 < fbLen - n) ? 1024 : fbLen - n;
+      client.write(fbBuf + n, chunkSize);
+      n += chunkSize;
     }
-    client.print(tail);
 
+    client.write(footer, sizeof(footer) - 1);
+
+    #ifdef DEBUG
     Serial.println("Waiting for server response...");
+    #endif
 
     esp_camera_fb_return(fb);
 
-    //boolean state = false;
-
-    // while ((startTimer + timeoutTimer) > millis()) {
-    //   Serial.print(".");
-    //   delay(10);
-    //   while (client.available()) {
-    //     char c = client.read();
-    //     if (c == '\n') {
-    //       if (getAll.length() == 0) {
-    //         state = true;
-    //       }
-    //       getAll = "";
-    //     } else if (c != '\r') {
-    //       getAll += String(c);
-    //     }
-    //     if (state == true) {
-    //       getBody += String(c);
-    //     }
-    //     startTimer = millis();
-    //   }
-    //   if (getBody.length() > 0) {
-    //     break;
-    //   }
+    // unsigned long timeoutEndTime = millis() + 10000;
+    // while (millis() < timeoutEndTime && !client.available()) {
+    //   // Wait for server response
     // }
-    //Serial.println();
+
     client.stop();
-    //Serial.println(getBody);
-  } 
-  else 
-  {
-    
+  } else {
     #ifdef DEBUG
     Serial.print("Connection to ");
     Serial.print(serverName);
-    getBody = "Connection failed.";
-    Serial.println(getBody);
+    Serial.println(" failed.");
     #endif
-
   }
-  return getBody;
 }
